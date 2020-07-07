9a34c4fbfa333cfbb5b38f3ccef4f29638f77a9c
YASUDA Jiro <portown09@gmail.com>
2020-07-07 12:14:23 +0900

    [Deploy] Fix deploying tool for Windows

--- deploy-dotfiles.bat
+++ deploy-dotfiles.bat
@@ -2,14 +2,15 @@
 
 setlocal
 
-set dst=%~dp0
-set src=%dst%dotfiles\
+set src=%~dp0
+set dst=%src%..\
 
-for %%i in (bin\git-branch-gc bin\git-branch-list bin\git-diff-commit .gitconfig .gitignore.global .tmux.conf .uncrustify.cfg .vim) do (
+for %%i in (bin\git-branch-gc bin\git-branch-list .gitconfig .gitignore.global .tmux.conf .uncrustify.cfg .vim) do (
     if exist %src%%%i\ (
         mklink /d %dst%%%i %src%%%i
     ) else (
         mklink %dst%%%i %src%%%i
     )
 )
+mklink /d %dst%vimfiles %src%.vim
 pause
